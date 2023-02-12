e01249abd19bb1c243b4e33f0d66e479a1fd363b
Asif419 <asshah419@gmail.com>
2023-02-13 01:07:31 +0600

    first verstion

--- js/math.js
+++ js/math.js
@@ -2,10 +2,19 @@ const firstNumber = document.getElementById('first-number');
 const symble = document.getElementById('operator');
 const secondNumber = document.getElementById('second-number');
 const finalNumber = document.getElementById('final-number');
-const btnsubmit = document.getElementById('btn-submit');
+const btnSubmit = document.getElementById('btn-submit');
+const btnReload = document.getElementById('btn-reload');
 const errorMessage = document.getElementById('error-message');
+const wrongMessage = document.getElementById('wrong-message');
+const streakField = document.getElementById('streak');
+
+
+
+
 const operators = ['+', '-', '*'];
 let result;
+let streak = 0;
+
 
 //generate number
 function generateNumber(min, max) {
@@ -14,38 +23,68 @@ function generateNumber(min, max) {
 
 }
 
+function one() {
+    //two number
+    let generatedFirstNumber = generateNumber(1, 20);
+    let generatedSecondNumber = generateNumber(1, 20);
 
-//two number
-let generatedFirstNumber = generateNumber(1, 20);
-let generatedSecondNumber = generateNumber(1, 20);
-
-//generate operator
-const operator = operators[Math.floor(Math.random() * operators.length)];
+    //generate operator
+    const operator = operators[Math.floor(Math.random() * operators.length)];
 
 
-//question making error handling
-if (operator == '-' || operator == '/') {
-    if (generatedFirstNumber < generatedSecondNumber) {
-        [generatedFirstNumber, generatedSecondNumber] = [generatedSecondNumber, generatedFirstNumber];
+    //question making error handling
+    if (operator == '-' || operator == '/') {
+        if (generatedFirstNumber < generatedSecondNumber) {
+            [generatedFirstNumber, generatedSecondNumber] = [generatedSecondNumber, generatedFirstNumber];
+        }
     }
+
+    //result finding
+    result = eval(`${generatedFirstNumber} ${operator} ${generatedSecondNumber}`);
+
+
+    // console.log(result);
+    // console.log(generatedFirstNumber, operator, generatedSecondNumber);
+
+    //Show value in web page
+    firstNumber.innerText = generatedFirstNumber;
+    secondNumber.innerText = generatedSecondNumber;
+    symble.innerText = operator;
+
+
+    //make empty input box
+    finalNumber.value = '';
 }
 
-//result finding
-result = eval(`${generatedFirstNumber} ${operator} ${generatedSecondNumber}`);
 
 
-// console.log(result);
-// console.log(generatedFirstNumber, operator, generatedSecondNumber);
-
-//Show value in web page
-firstNumber.innerText = generatedFirstNumber;
-secondNumber.innerText = generatedSecondNumber;
-symble.innerText = operator;
+one();
 
 
-btnsubmit.addEventListener('click', function () {
-    submitedValue = btnsubmit.value;
+btnSubmit.addEventListener('click', function () {
+
+    const submitedValue = finalNumber.value;
+
     if (submitedValue == '') {
         errorMessage.classList.remove('hidden');
     }
+    else {
+        errorMessage.classList.add('hidden');
+        const submitedNumber = parseFloat(submitedValue);
+
+        if (result === submitedNumber) {
+            streak++;
+            one();
+            streakField.innerText = streak;
+        }
+        else {
+            wrongMessage.classList.remove('hidden');
+            streak = 0
+            streakField.innerText = streak;
+        }
+    }
+})
+
+btnReload.addEventListener('click', function () {
+    location.reload();
 })
